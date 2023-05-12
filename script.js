b7798df858d5ffc02d804048361c4cb7827d7498
ahabanova <ahabanova95@gmail.com>
2023-05-12 11:22:27 +0200

    Upravené cvičení 1

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const inboxElm = document.querySelector('#inbox')
-
 //01
-// fetch(`https://apps.kodim.cz/daweb/trening-api/apis/emails`)
-//   .then(response => response.json())
-//   .then((data) => {
-//     console.log(data)
-//     data.emails.forEach(email => {
-//       inboxElm.innerHTML += `
-//       <div class="email">
-//         <div class="email__head">
-//           <button class="email__icon email__icon--closed"></button>
-//           <div class="email__info">
-//             <div class="email__sender">${email.sender.name}</div>
-//             <div class="email__subject">${email.subject}</div>
-//           </div>
-//           <div class="email__time">${email.time}</div>
-//         </div>
-//         <div class="email__body"></div>
-//       </div>
-//         `
-//     });
-//   })
+fetch(`https://apps.kodim.cz/daweb/trening-api/apis/emails`)
+  .then(response => response.json())
+  .then((data) => {
+
+    inboxElm.innerHTML += data.emails
+      .map(email => `
+      <div class="email">
+          <div class="email__head">
+            <button class="email__icon email__icon--closed"></button>
+            <div class="email__info">
+              <div class="email__sender">${email.sender.name}</div>
+              <div class="email__subject">${email.subject}</div>
+            </div>
+            <div class="email__time">${email.time}</div>
+          </div>
+          <div class="email__body"></div>
+      </div>
+      `
+      )
+      .join('')
+  });
 
 //02
